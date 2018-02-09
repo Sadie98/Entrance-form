fdc7847f70ec0ffde3455f316bbadbe4c78ae598
Ульяна Иванова <avdeeva_121212@mail.ru>
2018-02-10 02:27:13 +0300

    entered page added, login works

--- form.php
+++ form.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -6,12 +7,14 @@
 </head>
 
 <body>
-
+<?php
+    session_start();
+?>
 <div id="container">
     <h1>
         Вход
     </h1>
-    <form class="mainform" >
+    <form class="mainform" method="post">
         <link rel="stylesheet" rev="stylesheet" type="text/css" href="styles.css"  />
         <p class="name">
             <input type="text" name="name" value="Имя пользователя" onblur="if(this.value.length == 0) this.value = 'Имя пользователя'" onfocus="if(this.value == 'Имя пользователя') this.value = '' "/>
@@ -25,9 +28,20 @@
             <input type="password" name="password" value="•••••••" onblur="if(this.value.length == 0) this.value = '•••••••'" onfocus="if(this.value == '•••••••') this.value = '' "/>
         </p>
         <p class="send" align="center">
-            <input type="submit" value="Войти"  />
+            <input type="submit" name="submit" value="Войти"  />
         </p>
+        <?php
+        $user = "user";
+        $pass = "password";
+        if(!empty($_POST["submit"])){
+            if($user == $_POST["name"] AND $pass == $_POST["password"]){
+                header("Location: http://localhost:63343/test_task/entered.html");
+            }else echo '<bad>Логин или пароль неверны!</bad>';
+        }
+        ?>
+
     </form>
 </div>
 </body>
+
 </html>
